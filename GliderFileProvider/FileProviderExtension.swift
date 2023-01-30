bf9028753c275a170036f2c24a0d960fff76c562
Antonio <antonio@openroad.es>
2023-01-30 19:17:02 +0100

    Add file provider basic structure

---
+++ GliderFileProvider/FileProviderExtension.swift
@@ -0,0 +1,20 @@
+//
+//  FileProviderExtension.swift
+//  GliderFileProvider
+//
+//  Created by Antonio García on 30/1/23.
+//
+
+import FileProvider
+
+class FileProviderExtension: NSFileProviderExtension {
+    override init() {
+        super.init()
+        
+    }
+    
+    
+    deinit {
+
+    }
+}
